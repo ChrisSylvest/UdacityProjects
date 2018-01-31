327f28d298c6f422e69ed81c0596e614f78b31c5
ChrisSylvest <35995427+ChrisSylvest@users.noreply.github.com>
2018-01-31 07:04:19 -0800

    Update CreateActivity.java

--- InventoryApp/app/src/main/java/com/example/android/inventoryapp/CreateActivity.java
+++ InventoryApp/app/src/main/java/com/example/android/inventoryapp/CreateActivity.java
@@ -6,18 +6,27 @@ import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.android.inventoryapp.data.InventoryContract;
 
+import java.io.IOException;
+
 import javax.crypto.spec.IvParameterSpec;
+
+import static com.example.android.inventoryapp.data.InventoryProvider.LOG_TAG;
 
 /**
  * Created by Chris on 1/30/2018.
@@ -31,15 +40,19 @@ public class CreateActivity extends AppCompatActivity  {
     private EditText mQuantityEditText;
     private EditText mPriceEditText;
     private EditText mEmailEditText;
+    private Uri mPicturePath;
+
+    private static final int PICK_IMAGE_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
 
+
+
         Intent intent = getIntent();
         mCurrentProductUri = intent.getData();
-
         mNameEditText = (EditText) findViewById(R.id.edit_name);
         mQuantityEditText = (EditText) findViewById(R.id.edit_quantity);
         mPriceEditText = (EditText) findViewById(R.id.edit_price);
@@ -53,24 +66,58 @@ public class CreateActivity extends AppCompatActivity  {
             }
         });
 
+        Button imageButton = (Button) findViewById(R.id.image_button);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select picture"),
+                        PICK_IMAGE_REQUEST);
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null
+                && data.getData() != null) {
+            Uri uri = data.getData();
+            mPicturePath = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                ImageView imageView = (ImageView) findViewById(R.id.create_product_image);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
 
     private void saveProduct() {
         String nameString = mNameEditText.getText().toString().trim();
         String quantityString = mQuantityEditText.getText().toString().trim();
         String priceString = mPriceEditText.getText().toString().trim();
         String emailString = mEmailEditText.getText().toString().trim();
+        String imageUri = mPicturePath.toString();
+
+
 
         ContentValues values = new ContentValues();
         values.put(InventoryContract.InventoryEntry.COLUMN_PRODUCT_NAME, nameString);
-        values.put(InventoryContract.InventoryEntry.COLUMN_PRODUCT_QUANTITY, quantityString);
-        values.put(InventoryContract.InventoryEntry.COLUMN_PRODUCT_PRICE, priceString);
-        values.put(InventoryContract.InventoryEntry.COLUMN_PRODUCT_SUPPLIER_EMAIL, emailString);
         int quantity = 0;
         if (!TextUtils.isEmpty(quantityString)) {
             quantity = Integer.parseInt(quantityString);
         }
         values.put(InventoryContract.InventoryEntry.COLUMN_PRODUCT_QUANTITY, quantity);
+        values.put(InventoryContract.InventoryEntry.COLUMN_PRODUCT_PRICE, priceString);
+        values.put(InventoryContract.InventoryEntry.COLUMN_PRODUCT_SUPPLIER_EMAIL, emailString);
+        values.put(InventoryContract.InventoryEntry.COLUMN_PRODUCT_IMAGE, imageUri);
+
 
         if (mCurrentProductUri == null) {
             Uri newUri = getContentResolver().insert(InventoryContract.InventoryEntry.CONTENT_URI,
